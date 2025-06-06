4334ab67364661fae6475960a2d30ed92c231b6b
daye886 <dye58951@gmail.com>
2025-06-06 11:12:41 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "cddaye"         # 用户名
+UUID = "ba7bd8b7-0f22-4b8a-aa2e-2d18aa60d345"                     # UUID，留空则自动生成
+PORT = 49995                   # Vmess端口，留空或0则自动生成
+DOMAIN = "https://ye.yemener.cloudns.be"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMzZkMjhiOTBlNDM0MTM2OTc1ODE3MDY3N2IzODNkMjQiLCJ0IjoiMjliZDk2MzMtZGEwOC00MGRmLTk2NDEtMzM5ZTEzYmI4MTA0IiwicyI6Ik5qTmhNMlkyWW1VdE1XUm1aaTAwTmpBNUxUazFNVEF0TW1JNE5HTXlPVE5oTkdabSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
